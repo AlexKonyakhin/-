5877f392feeb52b7dbf9310043a847a274b9b4b7
Mr. Alex <alex_konyakhin@mail.ru>
2023-02-12 21:08:51 +0300

    сделал домашку

---
+++ Homeworks/Homework1/Program.cs
@@ -0,0 +1,81 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.WriteLine("Введите число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if(a > b)
+// {
+//     Console.WriteLine("Maximum is" + " " + a);
+
+// }
+// else
+// {
+//     Console.WriteLine("Maximum is" + " " + b);
+// }
+
+
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих
+
+// Console.WriteLine("Введите первое число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+
+// if(b > max)
+// {
+//     max = b;
+// }
+
+// if(c > max)
+// {
+//    max = c; 
+// }
+
+// Console.WriteLine("Maximum is" + " " + max);
+
+
+//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num % 2 == 0)
+// {
+//     Console.WriteLine($"Number of {num} is even");
+// }
+// else
+// {
+//     Console.WriteLine($"Number of {num} is odd");
+// }
+
+
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+
+// Console.WriteLine("Input number greater than one");
+
+// int num = Convert.ToInt32(Console.ReadLine());
+// int current = 2;
+
+// while (current <= num && current > 1)
+
+// {
+//     if (num % 2 == 0)
+//     {
+//         Console.Write(current + " ");
+//     }
+//     else
+//     {
+//         if (num % 2 == 1)
+//         {
+//             Console.Write(current + " ");
+//         }
+//     }
+//     current = current + 2;
+
+// }
